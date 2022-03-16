bcb4db844c51f2c3ff11ced3da6056f2e5cfc2fd
Timothy von Hollen <timothyvonhollen@gmail.com>
2022-03-16 11:22:56 -0700

    refactored project

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,13 @@
 import { MetarService } from "./weather/metar/metar.service.js";
-import {Parser} from 'xml2js';
-import { mapMetar } from "./weather/metar/mappers/metar-mapper.js";
-
-
 
 
 export async function multiply(x : number, y : number) {
   return x*y;
 }
 
-const jsonParser = new Parser();
-
-
 const ms = new MetarService();
-
-ms.getMetar(1, ['KBUR']).then((value) => {
-
-  jsonParser.parseString(value.data, (error, result) => {
-
-    const metar = result.response.data[0]
-    const mappedMetar = mapMetar(metar.METAR[0]);
-    console.log(JSON.stringify(mappedMetar));
-
-    if(error) {
-      console.log(error);
-    }
-
-
-  });
-
+ms.getMetar(1, ["KBUR"]).then((result) => {
+  console.log(result);
 });
+
+
--- src/weather/metar/metar.service.ts
+++ src/weather/metar/metar.service.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { Parser } from "xml2js";
+import { mapMetar } from "./mappers/metar-mapper.js";
 
 export class MetarService {
 
@@ -6,17 +8,36 @@ export class MetarService {
 
   async getMetar(hoursBefore: number, stations: string[]) {
 
-    return axios
-      .get(this.endpoint, {
-        params: {
-          requestType: "retrieve",
-          format: "xml",
-          stationString: stations.join("%20"),
-          hoursBeforeNow: hoursBefore
-        }
-      });
+    return new Promise((resolve) => {
 
+      axios
+        .get(this.endpoint, {
+          params: {
+            requestType: "retrieve",
+            format: "xml",
+            stationString: stations.join("%20"),
+            hoursBeforeNow: hoursBefore
+          }
+        })
+        .then((result) => {
+
+          const jsonParser = new Parser();
+
+          jsonParser.parseString(result.data, (error, result) => {
+
+            const metar = result.response.data[0]
+            const mappedMetar = mapMetar(metar.METAR[0]);
+            // console.log(JSON.stringify(mappedMetar));
+            resolve(mappedMetar);
+
+            if (error) {
+              console.log(error);
+            }
+          });
+
+        });
+    });
 
   }
-
 }
+
