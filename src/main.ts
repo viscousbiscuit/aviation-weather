24e882bbfe8a32f93a4bbbac2fa73b9f88e1aaab
Timothy von Hollen <timothyvonhollen@gmail.com>
2022-03-16 21:53:34 -0700

    updated main so tests pass

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
-import { MetarService } from "./weather/metar/metar.service.js";
-
 
 export async function multiply(x : number, y : number) {
   return x*y;
 }
-
-const ms = new MetarService();
-ms.getMetar(1, ["KBUR"]).then((result) => {
-  console.log(result);
-});
 
 
