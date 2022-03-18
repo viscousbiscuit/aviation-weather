2d0f202622b1926786a5890d7c963c4130d04df7
Timothy von Hollen <timothyvonhollen@gmail.com>
2022-03-18 09:22:13 -0700

    adding express

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import  express  from 'express';
+const app = express()
+const port = 3000
 
-export async function multiply(x : number, y : number) {
-  return x*y;
+export async function multiply(x: number, y: number) {
+  return x * y;
 }
 
+app.get('/', (req, res) => {
+  res.send('Working');
+})
 
-console.log('test');
+app.listen(port);
